862d1f8a706f2bd4d2c197386c3d48f08d7faf81
BinhTran <fam.out16@gmail.com>
2024-08-12 22:19:58 +0700

    Add cac files cho bai thuc hanh tim uoc chung lon nhat

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SUDUNGCAULENHLAP
+{
+    class TimUocChungLonNhat
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap hai so can tim uoc chung lon nhat:");
+            int a = Math.Abs(int.Parse(Console.ReadLine()));
+            int b = Math.Abs(int.Parse(Console.ReadLine()));
+
+            if (a == 0 || b == 0)
+            {
+              Console.WriteLine("Khong tim duoc uoc chung lon nhat");
+            }
+            else
+            {
+              while (a != b)
+                {
+                    if (a > b)
+                    {
+                        a = a - b;
+                    }
+                    else
+                    {
+                        b = b - a;
+                    }
+                }
+              Console.WriteLine("Uoc chung lon nhat bang: " + a);
+            }
+      }
+  }
+}
