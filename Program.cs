c4d0cb5b71a3a7a51d42e0665df87a443e4e5a1e
BinhTran <fam.out16@gmail.com>
2024-08-12 22:30:37 +0700

    Them dieu kien

--- Program.cs
+++ Program.cs
@@ -10,9 +10,17 @@ namespace SUDUNGCAULENHLAP
             int a = Math.Abs(int.Parse(Console.ReadLine()));
             int b = Math.Abs(int.Parse(Console.ReadLine()));
 
-            if (a == 0 || b == 0)
+            if (a == 0 && b == 0)
             {
               Console.WriteLine("Khong tim duoc uoc chung lon nhat");
+            }
+            else if (a==0&&b!=0)
+            {
+              Console.WriteLine("Uoc chung lon nhat bang: "+b);
+            }
+            else if (b==0&&a!=0)
+            {
+              Console.WriteLine("Uoc chung lon nhat bang: "+a);
             }
             else
             {
@@ -20,11 +28,11 @@ namespace SUDUNGCAULENHLAP
                 {
                     if (a > b)
                     {
-                        a = a - b;
+                      a = a - b;
                     }
                     else
                     {
-                        b = b - a;
+                      b = b - a;
                     }
                 }
               Console.WriteLine("Uoc chung lon nhat bang: " + a);
